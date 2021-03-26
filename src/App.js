d621dec4362426df0757b7f1be9c05c83e1eef06
K4sik <kasiks597@gmail.com>
2021-03-26 18:03:33 +0200

    Added fontawesome

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import TeamBoard from "./components/TeamBoard";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddTeam from "./components/Team/AddTeam";
 
 class App extends Component {
   render(){
     return (
-      <div className="App">
-        <h1 className="alert alert-warning">Welcome to our security agency</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/team" component={TeamBoard} />
+          <Route exact path="/team/addTeam" component={AddTeam} />
+        </div>
+      </Router>
     );
   }  
 }
