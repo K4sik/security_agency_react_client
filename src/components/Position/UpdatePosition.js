df598ee1dd59219fd7471c83e5226d0cdf44974b
K4sik <kasiks597@gmail.com>
2021-04-03 00:08:05 +0300

    Added Update page

---
+++ src/components/Position/UpdatePosition.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import classnames from "classnames";
+
+class UpdatePosition extends Component {
+    render() {
+        return (
+            <div className="addPosition">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <Link to="/position" className="btn btn-light">
+                                Back to Board
+                            </Link>
+                            <h4 className="display-4 text-center">Add Position</h4>
+                            <form onSubmit={this.onSubmit}>
+                                <div className="form-group">
+                                    <input 
+                                        type="text" 
+                                        className={classnames("form-control form-control-lg", {
+                                           // "is-invalid": errors.name
+                                        })} 
+                                        name="name" 
+                                        // value={this.state.name} 
+                                        placeholder="Position name" 
+                                        // onChange={this.onChange} 
+                                    />
+                                    {
+                                        // errors.name && (
+                                        //     <div className="invalid-feedback">{ errors.name }</div>
+                                        // )
+                                    }
+                                </div>
+                                <div className="form-group">
+                                    <textarea 
+                                        className="form-control form-control-lg" 
+                                        name="description" 
+                                        // value={this.state.description} 
+                                        placeholder="Description" 
+                                        // onChange={this.onChange} 
+                                        >
+                                    </textarea>
+                                </div>
+                                <input type="submit" className="btn btn-primary btn-block mt-4" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UpdatePosition
