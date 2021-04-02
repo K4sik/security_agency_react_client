92670fd0189f3418398f79ae197e995b06cf4b3b
K4sik <kasiks597@gmail.com>
2021-04-03 00:01:05 +0300

    Added PaymentTypeItem

--- src/components/PaymentTypeBoard.js
+++ src/components/PaymentTypeBoard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import PaymentTypeItem from './PaymentType/PaymentTypeItem';
 
 class PaymentTypeBoard extends Component {
     render() {
@@ -12,25 +13,9 @@ class PaymentTypeBoard extends Component {
                 <hr />
                 <div className="container">
                     <div className="row">
-                            <div className="card mb-1 bg-light">
-
-                                <div className="card-header text-primary">
-                                    ID: payment_type_id
-                                </div>
-                                <div className="card-body bg-light">
-                                    <h5 className="card-title">payment name</h5>
-                                    <p className="card-text text-truncate ">
-                                        description
-                                    </p>
-                                    <Link to="" className="btn btn-primary">
-                                        View / Update
-                                    </Link>
-
-                                    <button className="btn btn-danger ml-4">
-                                        Delete
-                                    </button>
-                                </div>
-                            </div>
+                        <PaymentTypeItem />
+                        <PaymentTypeItem />
+                        <PaymentTypeItem />
 
                         
                     </div>
