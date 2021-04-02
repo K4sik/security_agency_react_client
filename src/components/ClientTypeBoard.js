2d247bf6160e394cc85c197df30be9c18f671ee4
K4sik <kasiks597@gmail.com>
2021-04-02 23:59:17 +0300

    Added ClientTypeItem

--- src/components/ClientTypeBoard.js
+++ src/components/ClientTypeBoard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
+import ClientTypeItem from './ClientType/ClientTypeItem';
 
 class ClientTypeBoard extends Component {
     render() {
@@ -12,30 +13,11 @@ class ClientTypeBoard extends Component {
                 <hr />
                 <div className="container">
                     <div className="row">
-                            <div className="card mb-1 bg-light">
-
-                                <div className="card-header text-primary">
-                                    ID: client_type_id
-                                </div>
-                                <div className="card-body bg-light">
-                                    <h5 className="card-title">client_type name</h5>
-                                    <p className="card-text text-truncate ">
-                                        description
-                                    </p>
-                                    <Link to="" className="btn btn-primary">
-                                        View / Update
-                                    </Link>
-
-                                    <button className="btn btn-danger ml-4">
-                                        Delete
-                                    </button>
-                                </div>
-                            </div>
-
-                        
+                        <ClientTypeItem />
+                        <ClientTypeItem />
+                        <ClientTypeItem />
                     </div>
                 </div>
-
             </div>
         );
     }
