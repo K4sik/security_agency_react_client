9b1668650da5105303406bad5ba77078880c3499
K4sik <kasiks597@gmail.com>
2021-04-03 23:00:59 +0300

    Added new type of actions

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,7 @@
 export const GET_ERRORS = "GET_ERRORS";
+
 export const GET_TEAMS = "GET_TEAMS";
 export const GET_TEAM = "GET_TEAM";
 export const DELETE_TEAM = "DELETE_TEAM";
+
+export const GET_POSITIONS = "GET_POSITIONS";
